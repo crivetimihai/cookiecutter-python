ce2a7d1fe674bd0ec8e05c945a9f5f7b67ee1656
Mihai Criveti <crmihai1@ie.ibm.com>
2020-05-28 06:27:16 +0100

    black and flake8

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -6,9 +6,10 @@ import os
 from distutils.command.clean import clean
 from setuptools import setup
 
+
 def parse_requirements(filename):
-    ''' Load requirements from a pip requirements file '''
-    with open(filename, 'r') as fd:
+    """ Load requirements from a pip requirements file """
+    with open(filename, "r") as fd:
         lines = []
         for line in fd:
             line.strip()
@@ -16,17 +17,9 @@ def parse_requirements(filename):
                 lines.append(line)
     return lines
 
-def parse_requirements(filename):
-    ''' Load requirements from a pip requirements file '''
-    with open(filename, 'r') as fd:
-        lines = []
-        for line in fd:
-            line.strip()
-            if line and not line.startswith("#"):
-                lines.append(line)
-    return lines
 
-requirements = parse_requirements('requirements.txt')
+requirements = parse_requirements("requirements.txt")
+
 
 class CustomClean(clean):
     """Custom clean command."""
@@ -47,10 +40,13 @@ class CustomClean(clean):
             bak=r"find ./ -type f -name '*.bak' -exec rm {} \;",
             sonar=r"rm -rf ./.scannerwork",
             coverage=r"rm .coverage*",
-            docs_build=r"rm -rf docs/build/")
+            docs_build=r"rm -rf docs/build/",
+        )
         for key, cmd in cmd_list.items():
             print("Cleanning {}".format(key))
             os.system(cmd)
 
 
-setup(cmdclass={'clean': CustomClean}, install_requires=requirements,)
+setup(
+    cmdclass={"clean": CustomClean}, install_requires=requirements,
+)
