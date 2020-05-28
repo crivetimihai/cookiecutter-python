6ed1cf0ff3b68d5fabb16425a22b0028339e6cb9
Mihai Criveti <crmihai1@ie.ibm.com>
2020-05-28 08:51:59 +0100

    clean

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -9,9 +9,9 @@ from setuptools import setup
 
 def parse_requirements(filename):
     """ Load requirements from a pip requirements file """
-    with open(filename, "r") as fd:
+    with open(filename, "r") as file_descriptor:
         lines = []
-        for line in fd:
+        for line in file_descriptor:
             line.strip()
             if line and not line.startswith("#"):
                 lines.append(line)
@@ -31,12 +31,12 @@ class CustomClean(clean):
         # Clean additional junk
         cmd_list = dict(
             DS_Store=r"find ./ -name '.DS_Store' -print0 | xargs -0 rm -f;",
-            pyc=r"find ./ -type f -name '*.pyc' -exec rm -rf {} \;",
-            htmlcov="find ./ -type d -name 'htmlcov' -exec rm -rf {} \;",
-            mypy=r"find ./ -type d -name '.mypy_cache' -exec rm -rf {} \;",
-            eggs=r"rm -rf .eggs *.egg-info",
-            pytest=r"rm -rf .pytest_cache",
             empty_dirs=r"find ./ -type d -empty -delete;",
+            htmlcov=r"find ./ -type d -name 'htmlcov' -exec rm -rf {} \;",
+            mypy=r"find ./ -type d -name '.mypy_cache' -exec rm -rf {} \;",
+            pyc=r"find ./ -type f -name '*.pyc' -exec rm -rf {} \;",
+            pytest=r"rm -rf .pytest_cache",
+            eggs=r"rm -rf .eggs *.egg-info",
             bak=r"find ./ -type f -name '*.bak' -exec rm {} \;",
             sonar=r"rm -rf ./.scannerwork",
             coverage=r"rm .coverage*",
